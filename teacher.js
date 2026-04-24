feceacbef7c3dbf1bae7ce347f5c50ad42de1312
leearo9528-creator <leearo9528@gmail.comhttps://github.com/leearo9528-creator/FLITgit config --global user.name>
2026-04-24 16:49:06 +0900

    fix: 드라마 클립 슬라이드를 PART 03 섹션 바로 다음으로 이동

--- teacher.js
+++ teacher.js
@@ -179,6 +179,15 @@ const SLIDES = [
     sub:'재난 현장에서 한정된 자원으로 최대한 많은 생명을 살리는 법'
   },
 
+  { type:'theory', period:2, tag:'드라마 클립', color:P2_COLOR,
+    title:'🎬 드라마로 보는 트리아지',
+    points:[
+      '실제 재난 현장에서 트리아지가 어떻게 이루어지는지 확인해봐요',
+    ],
+    video:'https://www.youtube.com/embed/K2zGb--VkbY',
+    note:'▶ 드라마 속 응급실 트리아지 장면'
+  },
+
   { type:'theory', period:2, tag:'개념', color:P2_COLOR,
     title:'트리아지란?',
     points:[
@@ -197,15 +206,6 @@ const SLIDES = [
       { color:'#16A34A', label:'GREEN',  sub:'비응급', desc:'경상. 순서를 기다려도 지장 없음.',      example:'가벼운 타박상, 단순 염좌' },
       { color:'#374151', label:'BLACK',  sub:'지연',   desc:'소생 불가능. 제한 자원 하 처치 불가.',  example:'심폐정지 + 소생 불가 징후' },
     ]
-  },
-
-  { type:'theory', period:2, tag:'드라마 클립', color:P2_COLOR,
-    title:'🎬 드라마로 보는 트리아지',
-    points:[
-      '실제 재난 현장에서 트리아지가 어떻게 이루어지는지 확인해봐요',
-    ],
-    video:'https://www.youtube.com/embed/K2zGb--VkbY',
-    note:'▶ 드라마 속 응급실 트리아지 장면'
   },
 
   { type:'activity', period:2, act:'tri', color:P2_COLOR,
